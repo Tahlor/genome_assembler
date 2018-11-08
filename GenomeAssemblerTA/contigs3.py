66af6f8250346ecd1f721a53315067c74b2a6efd
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2018-11-08 16:40:09 -0700

    merge

--- GenomeAssemblerTA/contigs3.py
+++ GenomeAssemblerTA/contigs3.py
@@ -1,6 +1,7 @@
 import collections, sys
 import os
 
+<<<<<<< HEAD
 d = {"A": "T", "T": "A", "G": "C", "C": "G"}
 
 def twin(text):
@@ -9,6 +10,9 @@ def twin(text):
         if t in d.keys():
             out += d[t]
     return out
+=======
+output = open("results.txt", "w")
+>>>>>>> af0a04d3263b6915edd6e2999632467d3ac05f92
 
 def kmers(seq, k):
     for i in range(len(seq) - k + 1):
@@ -98,13 +102,37 @@ def parse_fasta(path):
     return [t[:-1] for i, t in enumerate(text) if i % 2 == 1]
 
 
+<<<<<<< HEAD
 def main_call(path, depth=0, k_range = None):
+=======
+
+def calc_n50(contigs):
+    contigs.sort()
+    contigs.sort(key=len, reverse=True)
+    total_length = sum(len(contig) for contig in contigs)
+    half_length = total_length / 2
+    count = 0
+    for contig in contigs:
+        count += len(contig)
+        if count >= half_length:
+            return len(contig)
+
+
+def longest_contig(contigs):
+    if len(contigs) == 0:
+        return 0
+    return max([len(contig) for contig in contigs])
+
+
+def main_call(path, depth=0):
+>>>>>>> af0a04d3263b6915edd6e2999632467d3ac05f92
     """
     path: path of fasta file
     depth: kmers with frequence of less than "depth" are dropped (assumed to be errant)
     """
     reads = parse_fasta(path)
     print("\t K #CONTIGS CONTIG_LIST")
+<<<<<<< HEAD
     if k_range is None:
         k_range = range(13,39,2)
     for k in k_range:
@@ -112,6 +140,18 @@ def main_call(path, depth=0, k_range = None):
         G, cs = all_contigs(d, k)
         print("\t", k, len(cs), cs) # cs is list of contigs
         #checker(reads, cs)
+=======
+    output.write("\tK\t#CONTIGS\tN50\t\tLARGEST\t\t\tCONTIG_LIST \n")
+    for k in range(13,51,2):
+        d = build_de_bruijn(kmers, k, depth)
+        G, cs = all_contigs(d, k)
+        print("\t", k, len(cs), cs) # cs is list of contigs
+        output.write("\t" + str(k))
+        output.write("\t" + str(len(cs)))
+        output.write("\t\t\t" + str(calc_n50(cs)))
+        output.write("\t\t" + str(longest_contig(cs)))
+        output.write("\t\t\t\t" + str(cs) + "\n")
+>>>>>>> af0a04d3263b6915edd6e2999632467d3ac05f92
 
 def checker(reads, contigs):
     result = False
@@ -127,12 +167,14 @@ def checker(reads, contigs):
 def main_loop():
     data_path = r"../data"
     print(os.getcwd())
+    output.write(os.getcwd())
     files = os.listdir(data_path)
     files.sort()
 
     for f in files:
         if f[-6:] == ".fasta":
             print(f)
+<<<<<<< HEAD
             main_call(os.path.join(data_path, f), depth=1)
 
 def test():
@@ -146,3 +188,7 @@ def test():
 if __name__ == "__main__":
     #main_loop()
     test()
+=======
+            output.write(f + "\n")
+            main_call(os.path.join(data_path, f))
+>>>>>>> af0a04d3263b6915edd6e2999632467d3ac05f92
